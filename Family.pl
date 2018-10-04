757c40720116daa1f7a8f88412fcdd5776225328
HuynhDuc <16520240@gm.uit.edu.vn>
2018-10-04 17:33:15 +0700

    [10042018] update family.pl, add uncle, aunt, grand/., brother and sister

--- Family.pl
+++ Family.pl
@@ -48,6 +48,7 @@ parent(a,c).
 parent(a,d).
 parent(b,c).
 parent(b,d).
+parent(d,i).
 parent(f,i).
 parent(c,g).
 parent(c,h).
@@ -56,22 +57,83 @@ parent(e,h).
 parent(k,m).
 parent(e,m).
 parentInLaw(X,Y):- parent(X,Z), married(Z,Y).
-mother(X,Y):- (parent(X,Y); parentInLaw(X,Y)), sex(X, female).
-father(X,Y):- (parent(X,Y); parentInLaw(X,Y)), sex(X, male).
+mother(X,Y):- parent(X,Y), sex(X, female).
+father(X,Y):- parent(X,Y), sex(X, male).
+mortheInLaw(X,Y):- sex(X, female), parentInLaw(X,Y).
+fatherInLaw(X,Y):- sex(X, male), parentInLaw(X,Y).
 %child
 child(X,Y):- parent(Y,X).
 childInLaw(X,Y):- parentInLaw(Y,X).
-son(X,Y):- (child(X,Y) ; childInLaw(X,Y)), sex(X,male).
-daughter(X,Y):- (child(X,Y) ; childInLaw(X,Y)), sex(X, female).
+son(X,Y):- child(X,Y), sex(X,male).
+daughter(X,Y):- child(X,Y), sex(X, female).
+sonInLaw(X,Y):- sex(X,male), childInLaw(X,Y).
+daughterInLaw(X,Y):- sex(X, female), childInLaw(X,Y).
 
+% Brother and sister and half this
+brother(X,Y):- 
+    sex(X, male), 
+    mother(Z,X),
+    mother(Z,Y),
+    father(K,X),
+    father(K,Y),
+    X \= Y.
 
-brother(X,Y):- parent(Z,Y), parent(Z,X), sex(X, male).
-sister(X,Y):- parent(Z,Y), parent(Z,X), sex(X, female).
-% youngBrother(X,Y):- brother(X,Y), age(X,?) < age(Y,?);    
+sister(X,Y):- 
+    sex(X, female), 
+    mother(Z,X),
+    mother(Z,Y),
+    father(K,X),
+    father(K,Y),
+    X \= Y.
 
+halfBrother(X,Y):- 
+    sex(X, male), 
+    mother(Z,X),
+    mother(Z,Y),
+    father(K,X) \= father(K,Y),
+    \+ brother(X,Y),
+    X \= Y.
+halfSister(X,Y):- 
+    sex(X, female), 
+    mother(Z,X),
+    mother(Z,Y),
+    father(K,X) \= father(K,Y),
+    \+ sister(X,Y),
+    X \= Y.
+%youngBrother(X,Y):-
+%oldBrother(X,Y):-
 
+brotherInLaw(X,Y):- 
+    sex(X, male), (brother(Z,Y); sister(Z,Y)), married(X,Z);
+    sex(X, male), (brother(Z,H); sister(Z,H)), married(X,Z), married(H,Y);
+    sex(X, male), (brother(X,Z); sister(X,Z)), married(Z,Y).
+sisterInLaw(X,Y):- 
+    sex(X, female), (brother(Z,Y); sister(Z,Y)), married(X,Z);
+    sex(X, female), (brother(Z,H); sister(Z,H)), married(X,Z), married(H,Y);
+    sex(X, female), (brother(X,Z); sister(X,Z)), married(Z,Y).
 
+cousin(X,Y):- 
+    parent(M,X), parent(N,Y), (brother(M,N); sister(M,N)).  
+% grandfather and grandmother
 
+grandfather(X,Y):- sex(X, male), parent(X,Z), parent(Z,Y).
+grandmother(X,Y):- sex(X, female), parent(X,Z), parent(Z,Y).
+grandchild(X,Y):- grandfather(Y,X); grandmother(Y,X).
+
+% uncle and aunt
+uncle(X,Y):- (brother(X,Z), child(Y,Z));
+            sex(X,male),
+            (brother(K,Z); sister(K,Z)),
+            married(X,K),
+            child(Y,Z).
+aunt(X,Y):-  (sister(X,Z), child(Y,Z));
+            sex(X,female),
+            (brother(K,Z); sister(K,Z)),
+            married(X,K),
+            child(Y,Z).
+
+%nephew
+nephew(X,Y):- uncle(Y,X) ; aunt(Y,X).
 
 
 
